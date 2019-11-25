0125dda5a181a702a147ef6dd5564a6394455f7a
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2019-11-25 11:26:36 +0100

    Show instance for TAC Instructions

--- src/TAC/Compiler.hs
+++ src/TAC/Compiler.hs
@@ -9,10 +9,26 @@ import Latte.AbsLatte
 
 import TAC.Instruction
 
+type Regs = M.Map Ident Reg
+type Labels = M.Map Label Instruction
+
 -- Labels and Variables mapping
-type Env = (M.Map Label Instruction, M.Map Ident Reg)
+type Env = (Regs, Labels)
 -- (Next register, Next label)
 type State = (Reg, Label)
 
 type IExcept = ExceptT String IO
 type Compiler = RWST Env [Instruction] State IExcept
+
+-- Helper functions
+freshTemp :: Compiler Reg
+freshTemp = do
+  (r, l) <- get
+  modify $ \_ -> (r + 1, l)
+  return r
+
+freshLabel :: Compiler Label
+freshLabel = do
+  (r, l) <- get
+  modify $ \_ -> (r, l + 1)
+  return l
--- src/TAC/Instruction.hs
+++ src/TAC/Instruction.hs
@@ -9,7 +9,7 @@ data Instruction
   | ITrip Reg TOp IArg         -- x := op y
   | ICopy Reg IArg             -- x := y
   | IJmp Label                 -- goto L
-  | ICond IArg COp IArg Label  -- if x `op` y goto L
+  | ICond COp IArg IArg Label  -- if x `op` y goto L
   | IParam IArg                -- param x
   | ICall Reg Fun Integer      -- call f, n
   | IRet IArg                  -- return x
@@ -23,3 +23,40 @@ data IArg
 data QOp = QOpAdd | QOpSub | QOpMul | QOpDiv | QOpAnd | QOpOr | QOpXor
 data TOp = TOpNeg | TOpNot
 data COp = CLt | CGt | CEq | CLe | CGe | CNe
+
+instance Show Instruction where
+  show (IQuad x op y z) = "t" ++ (show x) ++ " := " ++ (show y) ++ " " ++ (show op) ++ " " ++ (show z)
+  show (ITrip x op y) = "t" ++ (show x) ++ " := " ++ (show op) ++ " " ++ (show y)
+  show (ICopy x y) = "t" ++ (show x) ++ " := " ++ (show y)
+  show (IJmp l) = "goto L" ++ (show l)
+  show (ICond op x y l) = "if " ++ (show x) ++ " " ++ (show op) ++ " " ++ (show y) ++ " goto L" ++ (show l)
+  show (IParam x) = "param " ++ (show x)
+  show (ICall x f n) = "t" ++ (show x) ++ " := call " ++ f ++ ", " ++ (show n)
+  show (IRet x) = "return " ++ (show x)
+  show (IPut x i y) = "t" ++ (show x) ++ "[" ++ (show i) ++ "] := " ++ (show y)
+  show (IGet x y i) = "t" ++ (show x) ++ " := " ++ (show y) ++ "[" ++ (show i) ++ "]"
+
+instance Show IArg where
+  show (IArgReg x) = "t" ++ (show x)
+  show (IArgVal n) = (show n)
+
+instance Show QOp where
+  show QOpAdd = "+"
+  show QOpSub = "-"
+  show QOpMul = "*"
+  show QOpDiv = "/"
+  show QOpAnd = "and"
+  show QOpOr = "or"
+  show QOpXor = "xor"
+
+instance Show TOp where
+  show TOpNeg = "-"
+  show TOpNot = "not"
+
+instance Show COp where
+  show CLt = "<"
+  show CGt = ">"
+  show CEq = "=="
+  show CLe = "<="
+  show CGe = ">="
+  show CNe = "!="
